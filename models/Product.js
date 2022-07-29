92cb01226cd81cafb53f36e85b92c6e34da00ab2
Jordan  McClure <jordanmcclure@Jordans-MacBook-Air.local>
2022-07-28 21:16:27 -0500

    dependiencies and building models

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    category_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: Category,
+        key: "id"
+      }
+    }
     // define columns
   },
   {
