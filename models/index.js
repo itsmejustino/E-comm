b9f0c731d6ea31b2175065e21f2d2f143eeb428f
jdog <thuz3rfr13ndly@gmail.com>
2022-08-03 14:40:12 -0600

    correct through syntax

--- models/index.js
+++ models/index.js
@@ -1,51 +1,42 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.hasOne(Category,
-  {
-    foreignKey: 'category_id',
-    onDelete: 'CASCADE',
-  },
-);
+Product.hasOne(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-Category.hasMany(Product,
-{
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-}
-)
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 // Tags have many Tags
-Tag.hasMany(Product,
-{
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-}
-)
+Tag.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 // Products has many Products
-ProductTag.hasMany(Product,
-{
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-}
-)
+ProductTag.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag},
-{
- 
-  onDelete: 'CASCADE',
-}
-)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+
+  onDelete: "CASCADE",
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag},
-{
-  onDelete: 'CASCADE',
-}
-)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+
+  onDelete: "CASCADE",
+});
 module.exports = {
   Product,
   Category,
