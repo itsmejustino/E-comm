2688561bbc72b707e7a6d4ad710c5961c1262958
jdog <thuz3rfr13ndly@gmail.com>
2022-08-03 14:20:51 -0600

    async in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // find all products
     const productsData = await Product.findAll({ include: [{ Category }, { Tag }] })
@@ -102,7 +102,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Category.destroy({
